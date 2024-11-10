a3093030d70b478ba0d0bd85d649904505aebf97
malek-optimumpartners <malek.tubaishat@optimumpartners.co>
2024-11-10 13:57:29 +0300

    main

---
+++ tests/shared_context.ts
@@ -0,0 +1,13 @@
+export class SharedContext {
+    public newEmployee: {
+      id: string; 
+      credentials: { username: string; password: string };
+    };
+  
+    constructor() {
+      this.newEmployee = {
+        id: '0',
+        credentials: { username: '', password: '' },
+      };
+    }
+  }
