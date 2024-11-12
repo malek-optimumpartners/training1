58adf9a40942547b64af3b8d523f54879e0e5186
malek-optimumpartners <malek.tubaishat@optimumpartners.co>
2024-11-12 18:25:41 +0300

    main

--- tests/dashboardPage.ts
+++ tests/dashboardPage.ts
@@ -1,4 +1,4 @@
-import { Page, Locator } from "@playwright/test";
+import { Page } from "@playwright/test";
 import { LocatorsList } from "./locatorsList";
 
 export default class DashboardPage extends LocatorsList {
