7971a9011945e9345f3395647b842d82464f706e
malek-optimumpartners <malek.tubaishat@optimumpartners.co>
2024-11-14 16:00:26 +0300

    main

--- tests/dashboardPage.ts
+++ tests/dashboardPage.ts
@@ -9,6 +9,9 @@ export default class DashboardPage {
   lastNameText: string = "Tubaishat";
   middleNameText: string = "abdel";
   employeeID: string;
+  adminUsername:string="maltub2";
+  adminPassword:string="abcd1234";
+  adminPasswordChange:string="abcd1234";
 
   bannerTitle: Locator;
   firstNameTextBox: Locator;
@@ -22,6 +25,22 @@ export default class DashboardPage {
   employeeSearchButton: Locator;
   employeeDeleteButton: Locator;
   employeeDeleteConfirmation: Locator;
+  adminButton: Locator;
+  adminUsernameTextBox:Locator;
+  searchButton:Locator;
+  deleteButton:Locator;
+  editButton:Locator;
+  notificationToast:Locator;
+  changePasswordCheckBox:Locator;
+  changePasswordTextBox:Locator;
+  changePasswordTextBoxConfirmation:Locator;
+  saveButton:Locator;
+  sortLastNameDropDown:Locator;
+  sortLastNameAscending:Locator;
+  sortLastNameDescending:Locator;
+  firstElementOfLastName:Locator;
+  lastElementOfLastName:Locator;
+  menuPIM:Locator;
 
   constructor(pageDomain: Page) {
     this.pageDomain = pageDomain;
@@ -59,6 +78,24 @@ export default class DashboardPage {
       this.employeeDeleteConfirmation = this.pageDomain.locator(
         "button.orangehrm-button-margin.oxd-button--label-danger"
       );
+      this.adminButton = this.pageDomain.locator(
+        'a[href="/web/index.php/admin/viewAdminModule"]'
+      );
+      this.adminUsernameTextBox=this.pageDomain.locator('input.oxd-input--active[data-v-1f99f73c]:first-child');
+      this.searchButton=this.pageDomain.locator("button[type='submit']");
+      this.deleteButton=this.pageDomain.locator("i.bi-trash");
+      this.editButton=this.pageDomain.locator("i.bi-pencil-fill");
+      this.notificationToast=this.pageDomain.locator("p.oxd-text--toast-title");
+      this.changePasswordCheckBox=this.pageDomain.locator('label[data-v-6179b72a]');
+      this.changePasswordTextBox=this.pageDomain.locator(' div.oxd-grid-item.oxd-grid-item--gutters.user-password-cell > div > div:nth-child(2) > input');
+      this.changePasswordTextBoxConfirmation=this.pageDomain.locator(' div.oxd-form-row.user-password-row > div > div:nth-child(2) > div > div:nth-child(2) > input');
+      this.saveButton=this.pageDomain.locator('form > div.oxd-form-actions > button.oxd-button.oxd-button--medium.oxd-button--secondary.orangehrm-left-space');
+      this.sortLastNameDropDown=this.pageDomain.locator('div:nth-of-type(4) > div > i');
+      this.sortLastNameAscending=this.pageDomain.locator('div.orangehrm-container div:nth-of-type(4) li:nth-of-type(1)');
+      this.sortLastNameDescending=this.pageDomain.locator('div:nth-of-type(4) li:nth-of-type(2) > span');
+      this.firstElementOfLastName=this.pageDomain.locator('div.oxd-table-body > div:nth-of-type(1) div:nth-of-type(4) > div');
+      this.lastElementOfLastName=this.pageDomain.locator('div:nth-of-type(50) div:nth-of-type(4) > div');
+      this.menuPIM=this.pageDomain.locator('a[href="/web/index.php/pim/viewPimModule"]');
   }
 
 
--- tests/loginTest.spec.ts
+++ tests/loginTest.spec.ts
@@ -4,7 +4,6 @@ import DashboardPage from "../tests/dashboardPage";
 import { SharedContext } from "../tests/sharedContext";
 import { users } from "../tests/users";
 
-
 test("user_login", async ({ browser }) => {
   const context = await browser.newContext();
   const page = await context.newPage();
@@ -16,12 +15,13 @@ test("user_login", async ({ browser }) => {
     loggedPassword = "";
 
   // Add New Employee with Login Details
-  await page.goto(
-    "/web/index.php/pim/addEmployee"
-  );
+  await page.goto("/web/index.php/pim/addEmployee");
 
   for (const [key, { userName, passWord, status }] of Object.entries(users)) {
-    const passwordValidation = validatePassword(passWord,dashboardPage.regexFormat);
+    const passwordValidation = validatePassword(
+      passWord,
+      dashboardPage.regexFormat
+    );
     if (passwordValidation) {
       await loginPage.usernameTextBox.fill(userName);
       await loginPage.passwordTextBox.fill(passWord);
@@ -37,7 +37,7 @@ test("user_login", async ({ browser }) => {
         await expect(dashboardPage.bannerTitle).toHaveText("PIM");
       }
     }
-}
+  }
 
   await dashboardPage.firstNameTextBox.fill(dashboardPage.firstNameText);
   await dashboardPage.middleNameTextBox.fill(dashboardPage.middleNameText);
@@ -75,6 +75,140 @@ test("user_login", async ({ browser }) => {
   await dashboardPage.employeeDeleteButton.click();
   await dashboardPage.employeeDeleteConfirmation.click();
 });
+/*
+Part two
+
+
+Pre-steps:
+
+Create an admin user by a super admin user.
+Test Cases:
+
+1. Self-Deletion Restriction:
+    a. Verify that the newly created admin user cannot delete themselves from the system.
+    b. Ensure that the admin user's deletion attempt is denied with an appropriate error message.
+
+2. Password Change by Super Admin:
+    a. Verify that the super admin user can successfully change the password for the newly created admin user.
+    b. Ensure that the password change is reflected in the system and the newly created admin user can log in with the updated password.
+
+
+OrangeHRM
+https://opensource-demo.orangehrmlive.com
+using Playwright 
+3. Test sorting functionality based on market cap
+    a. by market cap (ascending).
+    b. by market cap (descending).
+    c. by market cap (default).
+
+4. Verify conditional formatting of percentage change in the last 1 hour (1H) column.
+    a. Green for positive values.
+    b. Red for negative values.
+    c. Black for zero values.
+*/
+test("Self-Deletion", async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  //point 1:
+  //login as admin
+  const loginPage = new LoginPage(page);
+  let dashboardPage = new DashboardPage(page);
+  await page.goto("/web/index.php/pim/addEmployee");
+  await loginPage.usernameTextBox.fill(dashboardPage.adminUsername);
+  await loginPage.passwordTextBox.fill(dashboardPage.adminPassword);
+  await loginPage.loginButton.hover();
+  await loginPage.loginButton.click();
+  //to click on admin button
+  await dashboardPage.adminButton.hover();
+  await dashboardPage.adminButton.click();
+  //to search for username maltub2
+  //fill username textbox
+  await dashboardPage.adminUsernameTextBox.fill(dashboardPage.adminUsername);
+  //click on search button
+  await dashboardPage.searchButton.hover();
+  await dashboardPage.searchButton.click();
+  await page.waitForTimeout(1000);
+  //try to delete
+  await dashboardPage.deleteButton.hover();
+  await dashboardPage.deleteButton.click();
+
+  //check message that deletion invalid
+  let count: number = 0;
+  while (count < 10) {
+    await page.waitForTimeout(100);
+    const str: string =
+      (await dashboardPage.notificationToast.textContent()) ?? "";
+    if (str != "<element(s) not found>") {
+      await expect(str).toBe("Error");
+      break;
+    }
+    count++;
+  }
+
+  //point 2:
+  //click on edit user
+  await Promise.all([
+    await dashboardPage.editButton.hover(),
+    await dashboardPage.editButton.click(),
+    //await page.waitForNavigation(),
+  ]);
+
+  //check box of change password?
+  await dashboardPage.changePasswordCheckBox.hover();
+  await dashboardPage.changePasswordCheckBox.focus();
+  await dashboardPage.changePasswordCheckBox.click();
+  await dashboardPage.changePasswordCheckBox.isVisible();
+  //fill password and the confirmation
+  await dashboardPage.changePasswordTextBox.hover();
+  await dashboardPage.changePasswordTextBox.focus();
+  await dashboardPage.changePasswordTextBox.click();
+  await dashboardPage.changePasswordTextBox.fill(dashboardPage.adminPasswordChange);
+  await dashboardPage.changePasswordTextBoxConfirmation.fill(dashboardPage.adminPasswordChange);
+  //click on save
+  await dashboardPage.saveButton.hover();
+  await dashboardPage.saveButton.click();
+  count  = 0;
+  //wait for the message confirmation
+  while (count < 10) {
+    await page.waitForTimeout(100);
+    const str: string =
+      (await dashboardPage.notificationToast.textContent()) ?? "";
+    if (str != "<element(s) not found>") {
+      await expect(str).toBe("Success");
+      break;
+    }
+    count++;
+  }
+});
+
+test("sorting", async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  //point 3:
+  //login as admin
+  const loginPage = new LoginPage(page);
+  let dashboardPage = new DashboardPage(page);
+  await page.goto("/");
+
+  await loginPage.usernameTextBox.fill(users.user4.userName);
+  await loginPage.passwordTextBox.fill(users.user4.passWord);
+  await loginPage.loginButton.hover();
+  await loginPage.loginButton.click();
+  await dashboardPage.menuPIM.click();
+  //Sort ascending
+  await dashboardPage.sortLastNameDropDown.click();
+  await dashboardPage.sortLastNameAscending.click();
+  const firstLastName:String=await dashboardPage.firstElementOfLastName.textContent()??"";
+  const lastLastName:String=await dashboardPage.lastElementOfLastName.textContent()??"";
+  await expect(lastLastName>firstLastName).toBeTruthy();
+  //Sort descending
+  await dashboardPage.sortLastNameDropDown.click();
+  await dashboardPage.sortLastNameDescending.click();
+  const firstLastName2:String=await dashboardPage.firstElementOfLastName.textContent()??"";
+  const lastLastName2:String=await dashboardPage.lastElementOfLastName.textContent()??"";
+  await expect(lastLastName2<firstLastName2).toBeTruthy();
+  
+});
 
 /**
  * Regex explanation:
@@ -86,6 +220,6 @@ test("user_login", async ({ browser }) => {
    @param password - the password to be used to validate it
    @param regExpFormat - the regex to be used to check validation
  */
-function validatePassword(password: string,regExpFormat:RegExp): boolean {
+function validatePassword(password: string, regExpFormat: RegExp): boolean {
   return regExpFormat.test(password);
 }
